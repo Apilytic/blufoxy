b8572d6d03e0005d37e30945a6e9284458e63069
Georgi Lambov <georgi.lambov@gmail.com>
2017-10-24 11:04:09 +0300

    Home page with logo.

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+var ElementScaler = {
+    e: $("#body-text"),
+    logo: $("#logo"),
+    documentWidth: $(document).width(),
+    documentHeight: $(document).height(),
+    baseLogo: {
+        'width': 379,
+        'height': 110
+    },
+
+    init: function () {
+        this.centerFooter();
+
+        if(this.documentWidth > 1200) {
+            //this.scaleLogo();
+        }
+
+        //this.centerLogo();
+        this.show();
+    },
+
+    show: function () {
+        this.logo.show();
+        this.e.show();
+    },
+
+    centerFooter: function () {
+        this.e.css('left', (this.documentWidth - this.e.width()) / 2);
+    },
+
+    centerLogo: function () {
+        this.logo.css('left', (this.documentWidth - this.logo.width()) / 2);
+    },
+
+    scaleLogo: function () {
+        if ((this.documentWidth / this.documentHeight) > (window.screen.width / window.screen.height)) {
+            this.logo.css('width', this.documentWidth * 0.32);
+
+            var xScaleFactor = this.baseLogo.width / this.logo.width();
+            var yScaleFactor = this.baseLogo.height / this.logo.height();
+            var scaleFactor = Math.min(xScaleFactor, yScaleFactor);
+
+            this.logo.css('top', 103 * scaleFactor);
+            this.logo.css('padding-top', 0);
+        }
+    }
+};
+
+//ElementScaler.init();
+
+$(window).resize(function () {
+  //ElementScaler.init();
+
+});
+
+setTimeout(function () {
+    //$("#logo img").show();
+
+}, 1000);
